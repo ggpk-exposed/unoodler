25a63c593bb2017c362bd806145c6f314cd83dff
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2024-12-29 18:00:41 +0700

    handle multi-block requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use serde::Deserialize;
+use std::num::ParseIntError;
+use std::str::FromStr;
 use worker::*;
 
 #[event(fetch)]
@@ -15,9 +17,9 @@ async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
 #[derive(Deserialize)]
 struct Params {
     url: String,
-    offset: usize,
-    compressed: usize,
-    extracted: usize,
+    offset: String,
+    compressed: String,
+    extracted: String,
     #[serde(default)]
     raw: bool,
 }
@@ -46,7 +48,48 @@ async fn handler(
         return Response::error("GET, HEAD only", 405);
     }
 
-    let response = get_data(url, offset, compressed)
+    let mut out_size = 0;
+    let mut start = None;
+    let mut end = None;
+    let blocks: std::result::Result<Vec<_>, Response> = offset
+        .split(",")
+        .map(usize::from_str)
+        .zip(compressed.split(",").map(usize::from_str))
+        .zip(extracted.split(",").map(usize::from_str))
+        .map(|((offset, compressed), extracted)| Ok((offset?, compressed?, extracted?)))
+        .map(|block: std::result::Result<_, ParseIntError>| {
+            let (offset, compressed, extracted) = block.map_err(|e| {
+                ResponseBuilder::new()
+                    .with_status(400)
+                    .fixed(format!("Bad numeric parameter: {:?}", e.kind()).into_bytes())
+            })?;
+            if let Some(prev_end) = end {
+                if offset != prev_end {
+                    return Err(ResponseBuilder::new()
+                        .with_status(400)
+                        .fixed("Non-contiguous blocks ending".as_bytes().to_vec()));
+                }
+            }
+            if start.is_none() {
+                start = Some(offset);
+            }
+            end = Some(offset + compressed);
+            out_size += extracted;
+            Ok(extracted)
+        })
+        .collect();
+
+    let blocks = match blocks {
+        Ok(blocks) => blocks,
+        Err(response) => return Ok(response),
+    };
+
+    let (start, end) = match (start, end) {
+        (Some(start), Some(end)) => (start, end - 1),
+        _ => return Response::error("missing parameters", 400),
+    };
+
+    let response = get_data(url, start, end)
         .await
         .map_err(|e| Error::Internal(format!("Upstream error {}", e).into()))?;
 
@@ -55,10 +98,14 @@ async fn handler(
         .get(reqwest::header::CONTENT_RANGE)
         .and_then(|v| v.to_str().ok());
 
-    if !content_range.is_some_and(|r| {
-        r.starts_with(format!("bytes {}-{}", offset, offset + compressed - 1).as_str())
-    }) {
-        return Response::error(format!("range header ignored: {:?}", content_range), 500);
+    if !content_range.is_some_and(|r| r.starts_with(format!("bytes {}-{}", start, end).as_str())) {
+        return Response::error(
+            format!(
+                "range header (bytes {}-{}) ignored: {:?}",
+                start, end, content_range
+            ),
+            500,
+        );
     }
 
     let headers = copy_headers(&response, accept_encoding);
@@ -73,16 +120,28 @@ async fn handler(
         );
     }
 
-    let mut result = vec![0; extracted];
-    match oozextract::Extractor::new()
-        .read_from_stream(&mut response.bytes_stream(), None, &mut result)
-        .await
-    {
-        Ok(_) => ResponseBuilder::new()
-            .with_headers(headers)
-            .from_bytes(result),
-        Err(e) => Response::error(format!("Decompression error: {}", e), 500),
+    let mut input = response.bytes_stream();
+    let mut output = vec![0; out_size];
+    let mut i = 0;
+    let mut remainder = None;
+    for extracted in blocks {
+        match oozextract::Extractor::new()
+            .read_from_stream(
+                &mut input,
+                remainder,
+                output.get_mut(i..i + extracted).unwrap(),
+            )
+            .await
+        {
+            Ok((_, r)) => remainder = r,
+            Err(e) => return Response::error(format!("Decompression error: {}", e), 500),
+        }
+        i += extracted;
     }
+
+    ResponseBuilder::new()
+        .with_headers(headers)
+        .from_bytes(output)
 }
 
 async fn head(url: &String) -> Result<Headers> {
@@ -118,18 +177,11 @@ fn copy_headers(response: &reqwest::Response, accept_encoding: Option<String>) -
     headers
 }
 
-async fn get_data(
-    url: String,
-    offset: usize,
-    compressed: usize,
-) -> reqwest::Result<reqwest::Response> {
+async fn get_data(url: String, start: usize, end: usize) -> reqwest::Result<reqwest::Response> {
     Ok(reqwest::Client::builder()
         .build()?
         .get(url)
-        .header(
-            reqwest::header::RANGE,
-            format!("bytes={}-{}", offset, offset + compressed - 1),
-        )
+        .header(reqwest::header::RANGE, format!("bytes={}-{}", start, end))
         .send()
         .await?)
 }
